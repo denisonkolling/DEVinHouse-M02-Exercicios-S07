687d0057b7e1e14438754d67598ea5436247aa0b
Denison Kolling <denisonkolling@gmail.com>
2023-12-13 11:02:16 -0300

    Ex 07 - Alteracao dados multas

---
+++ src/main/java/com/example/veiculosapi/utils/AlteracaoMulta.java
@@ -0,0 +1,25 @@
+package com.example.veiculosapi.utils;
+
+import com.example.veiculosapi.model.Multa;
+import com.example.veiculosapi.repository.MultaRepository;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AlteracaoMulta {
+
+    @Autowired
+    MultaRepository multaRepository;
+
+    @PostConstruct
+    public void alterarMulta() {
+
+        Multa multaDb = multaRepository.findById(3).get();
+
+        multaDb.setValor(380F);
+
+        multaRepository.save(multaDb);
+
+    }
+}
